5e8e2e0e0cae682001af2608ca2fc2a7556b4e5b
daVisionary <dtnathanson@gmail.com>
2017-05-19 21:54:59 -0700

    working Game-State object with agreed methods

--- Game-State.js
+++ Game-State.js
@@ -2,12 +2,56 @@
     "use strict";
 
     gameState = {
-        word: ""
-        getIncorrectGuesses: function() { }
-        lettersGuessed: []
-        makeGuessArray: function() { }
-        addLettersGuessed: function() { }
-        hiddenLetterChar: "_"
+        word: "",
+        lettersGuessed: [],
+        getIncorrectGuesses: function() {
+          var incorrectGuesses = 0;
+          for (let i = 0; i < lettersGuessed.length; i++) {
+            if ( word.indexOf(lettersGuessed[i]) == -1 ) {
+               incorrectGuesses++ ;
+            }
+          }
+          return incorrectGuesses;
+        },
+        makeGuessArray: function() {
+          var guessArray = [];
+          for (let i = 0; i < word.length; i++) {
+            let nextLetter = "";
+            if ( lettersGuessed.findIndex(word[i] > -1 ) {
+              nextLetter = word[i];
+            } else {
+               nextLetter = "_";
+            }
+            guessArray.push(nextLetter);
+          }
+          return guessArray;
+        },
+        addLettersGuessed: function(letter) {
+          lettersGuessed.push(letter);
+        },
+        makeValidLetterArray: function() {
+          var alphabet = 'ABCDEFGHIJKLMNOPQRSTUVWXYZ';
+          var validLetterArray = [];
+          for (let i = 0; i < alphabet.length; i++) {
+            let nextLetter = "_";
+            if ( alphabet.findIndex(lettersGuessed[i]) == -1 ) {
+               nextLetter = alphabet[i];
+            }
+            validLetterArray.push(nextLetter);
+          }
+          return validLetterArray;
+        },
+        // multi-function to replace makeGuessArray & makeValidLetterArray
+        showLettersInArray: function(sampleStr, foundArray, showBool) {
+          var differenceArray = [];
+          for (let i = 0; i < sampleStr.length; i++) {
+            let nextChar = "";
+            if ( sampleStr.findIndex(foundArray[i]) == -1 ) {
+
+            }
+          }
+        }
+        // hiddenLetterChar: "_"
     }
 
 })(module.exports);
